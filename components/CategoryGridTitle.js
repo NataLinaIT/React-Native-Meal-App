591dc36076af4904f9558e8bc2906faf7a952022
nch <nch@anadeainc.com>
2023-01-23 22:01:09 +0100

    Initial setup

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,57 @@
+import { Pressable, Text, View, StyleSheet, Platform } from 'react-native'
+
+const CategoryGridTitle = ({title, color}) => {
+  return (
+    <View style={[styles.item]}>
+      <Pressable
+        style={({pressed}) => [
+          styles.button,
+          pressed? styles.buttonPressed : null
+        ]}
+        android_ripple={{color: '#cccccc'}}
+      >
+        <View style={[
+          styles.inner, 
+          {backgroundColor: color}
+        ]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
+
+export default CategoryGridTitle;
+
+const styles = StyleSheet.create({
+  item: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    elevation: 4,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android'? 'hidden' : 'visible',
+  },
+  button: {
+    flex: 1
+  },
+  buttonPressed: {
+    opacity: 0.5
+  },
+  inner: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import {FlatList} from 'react-native'
+import {CATEGORIES} from '../data'
+import CategoryGridTitle from '../components/CategoryGridTitle'
+
+
+const renderCategoryItem = (itemData) => {
+  return <CategoryGridTitle
+            title={itemData.item.title}
+            color={itemData.item.color}
+          />
+}
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  )
+}
+
+export default CategoriesScreen
